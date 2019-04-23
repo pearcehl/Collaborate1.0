fae550ef67e0ef51963657f2effb977f8612a3f0
Galen R <49239182+Galen-R@users.noreply.github.com>
2019-04-23 17:06:04 -0400

    Created a Menu

--- GitHubApp/src/main.java
+++ GitHubApp/src/main.java
@@ -1,9 +1,11 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author henrypearce
@@ -14,15 +16,46 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("here's our assignment!");
-        System.out.println("I don't want to do this assignment");
-        System.out.println("Netbeans Test");
+        Scanner input = new Scanner(System.in);
+        boolean loop = true;
 
-        //Test 
-        //12345
-        //54321
-        System.out.println("Its a doozy");
+        int choice;
+        
+        while (true) {
+            System.out.println("Attack Menu\n");
+            System.out.println("----------------------------------------------------------------");
+            System.out.print("1.) Attack 1 \n");
+            System.out.print("2.) Attack 2 \n");
+            System.out.print("3.) End Attack \n");
+            System.out.print("\nEnter Your Menu Choice: ");
+
+            choice = input.nextInt();
+
+            switch (choice) {
+
+                case 1:
+
+                    System.out.print("Executing Attack 1");
+                    break;
+
+                case 2:
+
+                    System.out.println("Executing Attack 2");
+                    break;
+
+               
+                case 3:
+                    System.out.println("Ending Attack...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("This is not a valid Menu Option! Please Select Another");
+                    break;
+
+            }
+
+        }
+
     }
 
 }
