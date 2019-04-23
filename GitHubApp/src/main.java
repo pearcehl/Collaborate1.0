993642981501b6b7491fff58b4236b7f9bc83c55
Galen R <49239182+Galen-R@users.noreply.github.com>
2019-04-23 14:42:16 -0400

    Fixing all of the crap

--- GitHubApp/src/main.java
+++ GitHubApp/src/main.java
@@ -16,22 +16,10 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("here's our assignment!");
-<<<<<<< HEAD
-<<<<<<< HEAD
-        System.out.println("I don't want to do this assignment"); 
-=======
+        System.out.println("I don't want to do this assignment");
+
         System.out.println("Its a doozy");
 
-
->>>>>>> 4fc8480683f3df52f2ffbd2e59caa13474f3eae5
-=======
-        System.out.println("Its a doozy");
-
-
-=======
-        System.out.println("I don't want to do this assignment"); 
->>>>>>> b1f2a6dd2f71061c92c1c324c39ab53b6050acc8
->>>>>>> eba0b6fec43687deec76ea41bb9bcdaa51e9a82f
     }
 
 }
